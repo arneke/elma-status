35654556d17a0bea907c257aafe4571f5ffee53b
Arne Kepp <github@kepp.net>
2024-10-03 00:36:01 +0100

    Version 4, use directory.peppol.eu/search/1.0/json

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-/* Copyright 2020 Arne Kepp, Licensed under MIT */
-const got = require('got');
+/* Copyright 2024 Arne Kepp, Licensed under MIT */
+const got = require("got");
 
-const EHF_KEY = 'PEPPOLBIS_3_0_BILLING_01_UBL';
-const DIFI_API = 'https://hotell.difi.no/api/json/difi/elma/participants';
+// urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0::2.1
+const EHF_KEY = "poacc:billing";
+const PEPPOL_API = "https://directory.peppol.eu/search/1.0/json";
 
 function validateOrgNumber(orgNumber) {
   if (!/^([\d]{9})$/.test(orgNumber)) {
-    throw new Error('Organizational number must be 9 digits');
+    throw new Error("Organizational number must be 9 digits");
   }
 }
 
 function parseResponse(body, key) {
-  const entries = body.entries.filter((entry) => {
-    const ehfKey = entry[key];
-    if (ehfKey === undefined) throw new Error('Response format from DiFI was unexpected, key was not present.');
-    return (ehfKey === 'Ja');
+  const { matches } = body;
+  if (body.matches === undefined || matches.length !== 1) {
+    return false;
+    // throw new Error("Did not find organization in Peppol Directory.");
+  }
+  const [{ docTypes }] = matches;
+  const docType = docTypes.find(({ value }) => {
+    return value.includes(key);
   });
-  if(entries.length == 0) return false;
-  return entries[0];
+
+  return docType !== undefined && docType.value.includes("compliant");
 }
 
-module.exports = function(organizationalNumber, key = EHF_KEY) {
+module.exports = function (organizationalNumber, key = EHF_KEY) {
   const orgNumber = `${organizationalNumber}`;
   validateOrgNumber(orgNumber);
-
-  return got(DIFI_API, {
+  return got(PEPPOL_API, {
     searchParams: {
-      query: orgNumber,
-      page: '1',
+      q: orgNumber,
+      country: "NO",
     },
-    responseType: 'json',
-    resolveBodyOnly: true
-  })
-  .then((body) => parseResponse(body, key));
-}
+    responseType: "json",
+    resolveBodyOnly: true,
+  }).then((body) => parseResponse(body, key));
+};
